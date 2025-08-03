7fee79522172d23f392633a83d2c34d6f73fb3c8
Swapnil <swapnilshingne155@gmail.com>
2025-08-03 17:29:45 +0530

    Format code for consistency and readability in Ninja Training solution

--- Ninjas_Training.cpp
+++ Ninjas_Training.cpp
@@ -1,15 +1,15 @@
-#include<iostream>
-#include<bits/stdc++.h>
+#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
-int ninjaTrainingHelper(int ind, int last, vector<vector<int>>& points, vector<vector<int>>& dp)
+int ninjaTrainingHelper(int ind, int last, vector<vector<int>> &points, vector<vector<int>> &dp)
 {
-    if(ind == 0)
+    if (ind == 0)
     {
         int maxi = 0;
-        for(int i = 0; i < 3; i++)
+        for (int i = 0; i < 3; i++)
         {
-            if(i != last)
+            if (i != last)
             {
                 maxi = max(maxi, points[ind][i]);
             }
@@ -17,16 +17,16 @@ int ninjaTrainingHelper(int ind, int last, vector<vector<int>>& points, vector<v
         dp[ind][last] = maxi;
         return maxi;
     }
-    if(dp[ind][last] != -1)
+    if (dp[ind][last] != -1)
     {
         return dp[ind][last];
     }
     int maxi = 0;
-    for(int i = 0; i < 3; i++)
+    for (int i = 0; i < 3; i++)
     {
-        if(i != last)
+        if (i != last)
         {
-            int temp = points[ind][i] + ninjaTrainingHelper(ind-1, i, points, dp);
+            int temp = points[ind][i] + ninjaTrainingHelper(ind - 1, i, points, dp);
             maxi = max(maxi, temp);
         }
     }
@@ -34,13 +34,13 @@ int ninjaTrainingHelper(int ind, int last, vector<vector<int>>& points, vector<v
     return maxi;
 }
 
-int ninjaTrainingMemoization(int n, vector<vector<int>>& points)
+int ninjaTrainingMemoization(int n, vector<vector<int>> &points)
 {
     vector<vector<int>> dp(n, vector<int>(4, -1));
-    return ninjaTrainingHelper(n-1, 3, points, dp);
+    return ninjaTrainingHelper(n - 1, 3, points, dp);
 }
 
-int ninjaTrainingDp(int n, vector<vector<int>>& points)
+int ninjaTrainingDp(int n, vector<vector<int>> &points)
 {
     vector<vector<int>> dp(n, vector<int>(4, 0));
     dp[0][0] = max(points[0][1], points[0][2]);
@@ -48,21 +48,21 @@ int ninjaTrainingDp(int n, vector<vector<int>>& points)
     dp[0][2] = max(points[0][0], points[0][1]);
     dp[0][3] = max(points[0][0], max(points[0][1], points[0][2]));
 
-    for(int day = 1; day < n; day++)
+    for (int day = 1; day < n; day++)
     {
-        for(int last = 0; last < 4; last++)
+        for (int last = 0; last < 4; last++)
         {
             int maxi = 0;
-            for(int task = 0; task < 3; task++)
+            for (int task = 0; task < 3; task++)
             {
-                if(task != last)
+                if (task != last)
                 {
-                    int point = points[day][task] + dp[day-1][task];
+                    int point = points[day][task] + dp[day - 1][task];
                     maxi = max(maxi, point);
                 }
             }
             dp[day][last] = maxi;
         }
     }
-    return dp[n-1][3];
+    return dp[n - 1][3];
 }
