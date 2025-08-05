0871300ec535c338955d729381cca399e75c1a9d
Swapnil <swapnilshingne155@gmail.com>
2025-08-05 17:23:46 +0530

    Add Minimum Path Sum solution using dynamic programming

---
+++ Minimum_Path_Sum.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int minPathSumDp(vector<vector<int>> &grid)
+{
+    int m = grid.size();
+    int n = grid[0].size();
+    vector<vector<int>> dp(m, vector<int>(n, 0));
+    dp[0][0] = grid[0][0];
+    for (int i = 1; i < n; i++)
+    {
+        dp[0][i] = dp[0][i - 1] + grid[0][i];
+    }
+    for (int i = 1; i < m; i++)
+    {
+        dp[i][0] = dp[i - 1][0] + grid[i][0];
+    }
+    for (int i = 1; i < m; i++)
+    {
+        for (int j = 1; j < n; j++)
+        {
+            dp[i][j] = min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j];
+        }
+    }
+    return dp[m - 1][n - 1];
+}
+
+int minPathSum(vector<vector<int>> &grid)
+{
+    int m = grid.size();
+    int n = grid[0].size();
+    vector<vector<int>> dp(m, vector<int>(n, -1));
+    return helper(m - 1, n - 1, grid, dp);
+}
+
+int helper(int i, int j, vector<vector<int>> &grid, vector<vector<int>> &dp)
+{
+    if (i < 0 || j < 0)
+    {
+        return INT_MAX;
+    }
+    if (i == 0 && j == 0)
+    {
+        return grid[0][0];
+    }
+    if (dp[i][j] != -1)
+    {
+        return dp[i][j];
+    }
+    int up = helper(i - 1, j, grid, dp);
+    int left = helper(i, j - 1, grid, dp);
+    return dp[i][j] = min(up, left) + grid[i][j];
+}
