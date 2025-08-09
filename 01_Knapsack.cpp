f6f98f7f81f9ec0669ca399ad758dc4ff846af5b
Swapnil <swapnilshingne155@gmail.com>
2025-08-09 19:36:29 +0530

    Add a solution where we are solving the problem using Index not no. of items

--- 01_Knapsack.cpp
+++ 01_Knapsack.cpp
@@ -47,3 +47,36 @@ int knapsackDp(vector<int> &weight, vector<int> &value, int maxWeight)
     }
     return dp[n][maxWeight];
 }
+
+int knapsackHelper2(vector<int> &weight, vector<int> &value, int maxWeight, int ind, vector<vector<int>> &dp)
+{
+    if(ind == 0)
+    {
+        if(weight[0] <= maxWeight)
+        {
+            return value[0];
+        }
+        else{
+            return 0;
+        }
+    }
+    if (dp[ind][maxWeight] != -1)
+    {
+        return dp[ind][maxWeight];
+    }
+    int include = 0;
+    if (weight[ind] <= maxWeight)
+    {
+        include = value[ind] + knapsackHelper(weight, value, maxWeight - weight[ind], ind - 1, dp);
+    }
+    int exclude = knapsackHelper(weight, value, maxWeight, ind-1, dp);
+    dp[ind][maxWeight] = max(include, exclude);
+    return dp[ind][maxWeight];
+}
+
+int knapsack2(vector<int> &weight, vector<int> &value, int maxWeight)
+{
+    int n = weight.size();
+    vector<vector<int>> dp(n, vector<int>(maxWeight + 1, -1));
+    return knapsackHelper(weight, value, maxWeight, n-1, dp);
+}
