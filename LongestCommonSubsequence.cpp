916cc286b9d5c1caa0d7632a5afa134787ecf829
Swapnil <swapnilshingne155@gmail.com>
2025-08-13 13:59:14 +0530

    Add alternative implementation for Longest Common Subsequence using shifted indexing

--- LongestCommonSubsequence.cpp
+++ LongestCommonSubsequence.cpp
@@ -70,4 +70,24 @@ int longestCommonSubsequenceDp(string text1, string text2)
         }
     }
     return dp[n - 1][m - 1];
+
+    // Alternative  // now the index 0 is dp[1][i] and index -1 is dp[0][i]  /just shift the index right by 1
+    // int n = text1.length();
+    // int m = text2.length();
+    // vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+    // for (int i = 1; i <= n; i++)
+    // {
+    //     for (int j = 1; j <= m; j++)
+    //     {
+    //         if (text1[i - 1] == text2[j - 1])
+    //         {
+    //             dp[i][j] = 1 + dp[i - 1][j - 1];
+    //         }
+    //         else
+    //         {
+    //             dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+    //         }
+    //     }
+    // }
+    // return dp[n][m];
 }
